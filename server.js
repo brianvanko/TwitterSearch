f264bb2c108e49c37b69dc94a1b7d9918005d573
Brian Vanko <brianvanko@gmail.com>
2015-02-24 16:28:09 -0800

    init commit

---
+++ server.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(server);
+var Twit = require('twit');
+var config = require('./config');
+var searches = {};
+ 
+var T = new Twit({
+  consumer_key: config.CONSUMER_KEY,
+  consumer_secret: config.CONSUMER_SECRET,
+  access_token: config.ACCESS_TOKEN,
+  access_token_secret: config.ACCESS_TOKEN_SECRET
+});
+ 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  searches[socket.id] = {};
+  socket.on('q', function(q) {
+
+    if (!searches[socket.id][q]) {
+      console.log('New Search >>', q);
+
+      var stream = T.stream('statuses/filter', {
+        track: q
+      });
+
+      stream.on('tweet', function(tweet) {
+        //console.log(q, tweet.id);
+        socket.emit('tweet_' + q, tweet);
+      });
+
+      stream.on('limit', function(limitMessage) {
+        console.log('Limit for User : ' + socket.id + ' on query ' + q + ' has rechead!');
+      });
+
+      stream.on('warning', function(warning) {
+        console.log('warning', warning);
+      });
+
+      stream.on('reconnect', function(request, response, connectInterval) {
+        console.log('reconnect :: connectInterval', connectInterval)
+      });
+
+      stream.on('disconnect', function(disconnectMessage) {
+        console.log('disconnect', disconnectMessage);
+      });
+
+      searches[socket.id][q] = stream;
+    }
+  });
+
+  socket.on('remove', function(q) {
+    searches[socket.id][q].stop();
+    delete searches[socket.id][q];
+    console.log('Removed Search >>', q);
+  });
+
+  socket.on('disconnect', function() {
+    for (var k in searches[socket.id]) {
+      searches[socket.id][k].stop();
+      delete searches[socket.id][k];
+    }
+    delete searches[socket.id];
+    console.log('Removed All Search from user >>', socket.id);
+  });
+
+});
+
+server.listen(3000);
+console.log('Server listening on port 3000');
